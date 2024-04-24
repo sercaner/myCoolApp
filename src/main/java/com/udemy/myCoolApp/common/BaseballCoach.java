4224b5b645c174c629a118e03308508bf692091d
SercanEr <sercaner.221@gmail.com>
2024-04-24 20:46:49 +0300

    @Qualifier

--- src/main/java/com/udemy/myCoolApp/common/BaseballCoach.java
+++ src/main/java/com/udemy/myCoolApp/common/BaseballCoach.java
@@ -3,10 +3,10 @@ package com.udemy.myCoolApp.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
+public class BaseballCoach implements Coach{
 
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes";
+        return "Spend 30 minutes in batting practice";
     }
 }
--- src/main/java/com/udemy/myCoolApp/common/TennisCoach.java
+++ src/main/java/com/udemy/myCoolApp/common/TennisCoach.java
@@ -3,10 +3,10 @@ package com.udemy.myCoolApp.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
+public class TennisCoach implements Coach{
 
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes";
+        return "Practice your backhand volley";
     }
 }
