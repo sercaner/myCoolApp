fe4c749cce29644ab902e7d60ef2d359357b8053
SercanEr <sercaner.221@gmail.com>
2024-05-01 01:30:52 +0300

    crud and entityManager

--- src/main/java/com/udemy/myCoolApp/dao/StudentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.udemy.myCoolApp.dao;
-
-import com.udemy.myCoolApp.entity.Student;
-
-public interface StudentDAO {
-
-    void save(Student theStudent);
-    Student findById(Integer id);
-}
