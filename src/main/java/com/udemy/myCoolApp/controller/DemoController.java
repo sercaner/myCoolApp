2fad63a44c9640f7aa7a4c47f1070010d0cbe628
SercanEr <sercaner.221@gmail.com>
2024-05-16 21:50:44 +0300

    spring mvc security - custom tables with jpa/hibernate

---
+++ src/main/java/com/udemy/myCoolApp/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.udemy.myCoolApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome() {
+
+        return "home";
+    }
+
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders() {
+
+        return "leaders";
+    }
+
+    @GetMapping("/systems")
+    public String showSystems() {
+
+        return "systems";
+    }
+}
