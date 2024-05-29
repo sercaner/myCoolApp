8aedd57027fa71ea5ce3bcdd6c87b359c46ef9ef
SercanEr <sercaner.221@gmail.com>
2024-05-30 00:22:50 +0300

    combining pointcuts

--- src/main/java/com/udemy/myCoolApp/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/udemy/myCoolApp/aspect/MyDemoLoggingAspect.java
@@ -13,17 +13,28 @@ public class MyDemoLoggingAspect {
    @Pointcut("execution(* com.udemy.myCoolApp.dao.*.*(..))")
    private void forDaoPackage() {}
 
+    //create pointcut for getter methods
+    @Pointcut("execution(* com.udemy.myCoolApp.dao.*.get*(..))")
+    private void getter() {}
+
+    //create pointcut for setter methods
+    @Pointcut("execution(* com.udemy.myCoolApp.dao.*.set*(..))")
+    private void setter() {}
+
+    //create point: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
 
     //@Before("execution(public void add*())")
     //@Before("execution(void add*(com.udemy.myCoolApp.Account, ..))")
     //@Before("execution(* com.udemy.myCoolApp..add*(..))")
     //@Before("execution(* com.udemy.myCoolApp.dao.*.*(..))")//any access modifier any class any method any params
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n=====>>> Performing API analytics");
     }
