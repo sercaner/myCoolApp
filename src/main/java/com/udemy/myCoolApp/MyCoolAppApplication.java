daee47eb007ddbcb60ad718d87937450c40e0096
SercanEr <sercaner.221@gmail.com>
2024-06-01 00:16:43 +0300

    around advice rethrow exception

--- src/main/java/com/udemy/myCoolApp/MyCoolAppApplication.java
+++ src/main/java/com/udemy/myCoolApp/MyCoolAppApplication.java
@@ -32,8 +32,23 @@ public class MyCoolAppApplication {
 			//demoTheAfterThrowingAdvice(theAccountDAO);
 			//demoTheAfterAdvice(theAccountDAO);
 			//demoTheAroundAdvice(theAccountDAO, theTrafficFortuneService);
-			demoTheAroundAdviceHandleException(theTrafficFortuneService);
+			//demoTheAroundAdviceHandleException(theTrafficFortuneService);
+			demoTheAroundAdviceRethrowException(theTrafficFortuneService);
 		};
+	}
+
+	private void demoTheAroundAdviceRethrowException(TrafficFortuneService theTrafficFortuneService) {
+
+		System.out.println("\nMain Program: demoTheAroundAdviceRethrowException");
+
+		System.out.println("Calling getFortune()");
+
+		boolean tripWire = true;
+		String data = theTrafficFortuneService.getFortune(tripWire);
+
+		System.out.println("\nMy fortune is: " + data);
+
+		System.out.println("Finished");
 	}
 
 	private void demoTheAroundAdviceHandleException(TrafficFortuneService theTrafficFortuneService) {
